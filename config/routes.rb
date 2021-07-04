64dc599c85a4bd95929eed0b60822579877358e8
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-07-04 15:38:29 -0400

    removed nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   resources :advisors
-  resources :academic_plans
   resources :students do
     resources :academic_plans
-  end
-  resources :advisors do
-    resources :students
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

b5282495912fe03724e5f5444b275dc4eb301183
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-07-13 18:27:01 -0400

    added password digest column to Advisor migration

--- db/migrate/20210702184342_create_advisors.rb
+++ db/migrate/20210702184342_create_advisors.rb
@@ -4,6 +4,7 @@ class CreateAdvisors < ActiveRecord::Migration[6.1]
       t.string :ugamyid
       t.string :first_name
       t.string :last_name
+      t.string :password_digest
       t.references :college, null: false, foreign_key: true
 
       t.timestamps
